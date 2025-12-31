99db08881311c2f6c416b858cf05fa811d07e7de
varshini-1904 <varshinipalthineni@gmail.com>
2025-12-31 15:36:23 +0530

    removing server file

--- server.js
+++ server.js
@@ -1,15 +1,15 @@
-require("dotenv").config();
+// require("dotenv").config();
 
 
-require("dotenv").config();
-const app = require("./app");
-const { connectDB } = require("./db");
+// require("dotenv").config();
+// const app = require("./app");
+// const { connectDB } = require("./db");
 
-const PORT = 3000;
+// const PORT = 3000;
 
-(async () => {
-  await connectDB();
-  app.listen(PORT, () => {
-    console.log("Local server running on port", PORT);
-  });
-})();
+// (async () => {
+//   await connectDB();
+//   app.listen(PORT, () => {
+//     console.log("Local server running on port", PORT);
+//   });
+// })();
